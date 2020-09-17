2c27e0f82f1aadec1470a5b52fb74523515b2708
gsalas31 <grace_salas94@hotmail.com>
2020-09-17 13:29:49 -0400

    testing

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div>
+  <h1>HellO WORLD </h1>
+  <h2>{{cheese}}</h2>
+  <HelloWorld/>
+</div>
+
+
+
+
+
+  
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue"
 
-export default {
+export default{
   name: 'App',
-  components: {
-    HelloWorld
+  data: function(){
+    return{
+      cheese: "gouda"
+    }
+  },
+  components:{
+    HelloWorld,
   }
-}
+};
+
+
+
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+ h1{
+   color:green;
+ }
+
+
+
 </style>
